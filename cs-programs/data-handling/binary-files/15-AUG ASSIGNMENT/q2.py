7d32231f4ce044b60cf932425eee220017ac4543
Sujal Singh <email.sujalsingh@gmail.com>
2021-08-15 12:04:13 +0530

    15 AUG ASSIGNMENT QUESTION 2

--- cs-programs/data-handling/binary-files/15-AUG ASSIGNMENT/q2.py
+++ cs-programs/data-handling/binary-files/15-AUG ASSIGNMENT/q2.py
@@ -1,34 +1,3 @@
-# SCHOOL RESOURCES ARCHIVE
-
-All school related resources stored here.
-#### THIS WEBSITE IS MAINTAINED BY SUJAL SINGH
-
-### RESOURCE LIST
-
-##  Q1) WAF to create a binary file 'student.dat' storing the student details as roll number, name and the marks depending on the choice of the user.
-```python
-# Q1) WAF to create a binary file 'student.dat' storing the student details as
-# roll number, name and the marks depending on the choice of the user.
-import pickle
-
-
-def create():
-    file = open("student.dat", "wb")
-    while input("Enter values (y/n): ").lower() == "y":
-        print()
-        pickle.dump([
-            int(input("Enter roll number: ")),
-            input("Enter student's name: "),
-            float(input("Enter student's marks: "))
-        ], file)
-    file.close()
-
-
-create()
-
-```
-## <br>2) [PRACTICAL] Write a menu driven program to do the following functions:-<br>a) To create a binary file 'student.dat' containing fields as rno, name, marks and grade in a list.<br>b) Display the details of all those students who have got grade A.<br>
-```python
 """
 2) [PRACTICAL] Write a menu driven program to do the following functions:-
 a) To create a binary file 'student.dat' containing fields as rno, name, marks and grade in a list.
@@ -88,9 +57,3 @@ def menu():
 
 
 menu()
-
-```
-
-
-### ENCOUNTERED SOMETHING WRONG?
-Raise an issue on [github](https://github.com/sujaldev/school) to report anything.
